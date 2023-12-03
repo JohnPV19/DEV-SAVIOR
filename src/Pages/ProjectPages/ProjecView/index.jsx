fa8f6e7d86f8de1c235aed1c65dbf2038c56b4cb
brrrun <brunomsantosmendes@gmail.com>
2023-12-03 15:29:08 +0000

    Increased accepted file types

--- src/Pages/ProjectPages/ProjecView/index.jsx
+++ src/Pages/ProjectPages/ProjecView/index.jsx
@@ -6,15 +6,17 @@ import 'prismjs/themes/prism-okaidia.css';
 
 const API_URL = "http://localhost:5005";
 
+
 function ProjectView() {
   const { fileId } = useParams();
-  const [projectDetails, setProjectDetails] = useState([]);
+  const [projectDetails, setProjectDetails] = useState(null);
+
   const [projectName, setProjectName] = useState([]);
   const [projectContent, setProjectContent] = useState([]);
   const [projectSaveDate, setProjectSaveDate] = useState([]);
   const [, setForceUpdate] = useState(false);
 
-  useEffect(() => {
+  useEffect(() => { 
     let isMounted = true;
 
     axios
@@ -35,22 +37,67 @@ function ProjectView() {
       };
   }, [fileId]);
 
+
+  const getLanguageClass = (fileName) => {
+    const fileExtension = fileName.split('.').pop().toLowerCase();
+
+    // Map file extensions to Prism language classes
+    const languageMap = {
+      // Javascript and React related files
+      js: 'language-javascript', 
+        'package.json': 'language-json',
+        'package-lock.json': 'language-json',
+      jsx: 'language-jsx',
+      css: 'language-css',
+      html: 'language-html',
+      json: 'language-json',
+      ejs: 'language-html', 
+      pug: 'language-pug',
+      md: 'language-markdown',
+      scss: 'language-scss',
+      sass: 'language-sass',
+      less: 'language-less',
+      gitignore: 'language-git',
+      Dockerfile: 'language-dockerfile',
+        'docker-compose.yml': 'language-yaml',
+      // Other Languages files
+      py: 'language-python',  // Python
+      java: 'language-java',  // Java
+      cs: 'language-csharp',  // C#
+      cpp: 'language-cpp',  // C++
+      h: 'language-cpp', // Assuming header files are associated with C++ code
+      php: 'language-php',  // PHP
+      ts: 'language-typescript',  // TypeScript
+      tsx: 'language-tsx', 
+    };
+
+    // Use the mapped language class or default to 'language-text'
+    return languageMap[fileExtension] || 'language-text';
+  };
+
+
+
   return (
     <div>
       <h1>File Viewer</h1>
       {projectDetails &&
       <pre>
         <div>
-          <h3>File: </h3> <br />
-          <code className="language-javascript">{projectName}</code>
+        <h3>File: </h3> <br />
+            <code className={getLanguageClass(projectDetails.fileName)}>
+              {projectDetails.fileName}
+            </code>
         </div>
         <div>
-          <h3>Code: </h3>
-          <code className="language-javascript">{projectContent}</code>
+        <h3>Code: </h3>
+            <code
+              className={getLanguageClass(projectDetails.fileName)}
+              dangerouslySetInnerHTML={{ __html: projectDetails.content }}
+            />
         </div>
         <div>
-          <h3>Date uploaded: </h3>
-          <code className="language-javascript">{projectSaveDate}</code>
+        <h3>Date uploaded: </h3>
+            <code className="language-javascript">{projectDetails.saveDate}</code>
         </div>
     </pre>
       }
