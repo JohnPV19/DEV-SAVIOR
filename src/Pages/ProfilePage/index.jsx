d1dbb5d5aa45dd424e20d3cf8fad1009bbf897c5
João Valente <joao.valente5935@gmail.com>
2023-12-07 16:21:20 +0000

    Update searchbar

--- src/Pages/ProfilePage/index.jsx
+++ src/Pages/ProfilePage/index.jsx
@@ -36,87 +36,71 @@ function ProfilePage() {
         // Log user state after it's updated
         console.log('User Data:', user);
       }, [user]); // Add a separate useEffect for logging when user state changes
-  return (
-    <div>
-        <div>
-            {!user.avatar &&
-            <p>Show us your face</p>}
-            {user.avatar &&
-            <img src={avatarUrl} style={{ width: '100px', height: 'auto' }} />}
-        </div>
-        <div>
-            <h4>Username:</h4>
-            <p>{user.username}</p>
-        </div>
-        <div>
-            <h4>E-mail:</h4>
-            <p>{user.email}</p>
-        </div>
-        <div>
-            <h4>Name:</h4>
-            {!user.firstName &&
-            <p>Who are you?</p>}
-            {user.lastName &&
-            <p>{user.firstName} {user.lastName}</p>
-            }
-        </div>
-        <div>
-            <h4>Projects:</h4>
-            {user.createdProjects == [] &&
-            <p>Add your first project...</p>}
-            {user.createdProjects &&
-            user.createdProjects.map((project, index)=>{
-                return(
-                    <div key={index}>
+      return (
+        <div className="profile-container">
+          <div className="profile-image">
+            {!user.avatar && <p>Show us your face</p>}
+            {user.avatar && <img src={avatarUrl} alt="User Avatar" style={{ width: '100px', height: 'auto' }} />}
+          </div>
+          <div className="profile-details">
+            <div>
+              <h4>Username:</h4>
+              <p>{user.username}</p>
+            </div>
+            <div>
+              <h4>E-mail:</h4>
+              <p>{user.email}</p>
+            </div>
+            <div>
+              <h4>Name:</h4>
+              {!user.firstName && <p>Who are you?</p>}
+              {user.firstName && user.lastName && <p>{user.firstName} {user.lastName}</p>}
+            </div>
+            <div>
+              <h4>Projects:</h4>
+              {!user.createdProjects.length && <p>Add your first project...</p>}
+              {user.createdProjects.length > 0 &&
+                user.createdProjects.map((project, index) => (
+                  <div key={index}>
                     <Link to={`/api/project/${project._id}`}><p>{project.fileName}</p></Link>
-                    </div>
-                )
-            })}
-        </div>
-        <div>
-            <h4>Skills:</h4>
-            {!user.skills &&
-            <p>Show us your skills</p>}
-            {user.skills &&
-            (user.skills).map((skill, index)=>{
-                return(
-                    <div key={index}>
+                  </div>
+                ))}
+            </div>
+            <div>
+              <h4>Skills:</h4>
+              {!user.skills.length && <p>Show us your skills</p>}
+              {user.skills.length > 0 &&
+                user.skills.map((skill, index) => (
+                  <div key={index}>
                     <p>{skill}</p>
-                    </div>
-                )
-            })}
-        </div>
-        <div>
-            <h4>Interests:</h4>
-            {!user.interests &&
-            <p>What are you into?</p>}
-            {user.interests &&
-            (user.interests).map((interest, index)=>{
-                return(
-                    <div key={index}>
+                  </div>
+                ))}
+            </div>
+            <div>
+              <h4>Interests:</h4>
+              {!user.interests.length && <p>What are you into?</p>}
+              {user.interests.length > 0 &&
+                user.interests.map((interest, index) => (
+                  <div key={index}>
                     <p>{interest}</p>
-                    </div>
-                )
-            })}
-        </div>
-        <div>
-            <h4>Your Posts:</h4>
-            {!user.createdPosts &&
-            <p>You haven't created any posts</p>}
-            {user.createdPosts &&
-            user.createdPosts.map((post, index)=>{
-                console.log("test" , post.title)
-                return(
-                    <div key={index}>
+                  </div>
+                ))}
+            </div>
+            <div>
+              <h4>Your Posts:</h4>
+              {!user.createdPosts.length && <p>You haven't created any posts</p>}
+              {user.createdPosts.length > 0 &&
+                user.createdPosts.map((post, index) => (
+                  <div key={index}>
                     <Link to={`/api/posts/${post._id}`}><p>{post.title}</p></Link>
-                    </div>
-                )
-            })}
+                  </div>
+                ))}
+            </div>
+            <div>
+              <a href={`/profile/${_id}/edit`}><button>Edit Profile</button></a>
+            </div>
+          </div>
         </div>
-        <div>
-            <a href={`/profile/${_id}/edit`}><button>Edit Profile</button></a>
-        </div>
-    </div>
-  )
-}
+      );
+    }
 export default ProfilePage
